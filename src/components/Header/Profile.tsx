2cd20587e9feac7c9a7701603c21a438cf10e067
Fischer <fischerrobson@gmail.com>
2021-08-10 00:03:36 -0300

    header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Robson Fischer</Text>
-        <Text color="gray.300" fontSize="small">
-          fischerrobson@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Robson Fischer</Text>
+          <Text color="gray.300" fontSize="small">
+            fischerrobson@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Robson Fischer" src="https://github.com/FischerRobson.png" />
     </Flex>
   )
